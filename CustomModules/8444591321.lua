ab65a3ed94076660e9314df614e22e4d62042018
Neko <128447188+YukileNeko@users.noreply.github.com>
2024-11-08 14:23:24 -0500

    Update 8444591321.lua

--- CustomModules/8444591321.lua
+++ CustomModules/8444591321.lua
@@ -1,19 +1,19 @@
 local function vapeGithubRequest(scripturl)
-	if not isfile("vape/"..scripturl) then
-		local suc, res = pcall(function() return game:HttpGet("https://raw.githubusercontent.com/YukileNeko/Vape/"..readfile("vape/commithash.txt").."/"..scripturl, true) end)
-		if not suc or res == "404: Not Found" then return nil end
-		if scripturl:find(".lua") then res = "--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.\n"..res end
-		writefile("vape/"..scripturl, res)
-	end
-	return readfile("vape/"..scripturl)
+    if not isfile(scripturl) then
+        local suc, res = pcall(function() return game:HttpGet("https://raw.githubusercontent.com/YukileNeko/Vape/main/CustomModules/"..scripturl, true) end)
+        if not suc or res == "404: Not Found" then return nil end
+        if scripturl:find(".lua") then res = "--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.\n"..res end
+        writefile(scripturl, res)
+    end
+    return readfile(scripturl)
 end
 
 shared.CustomSaveVape = 6872274481
-if isfile("vape/CustomModules/6872274481.lua") then
-	loadstring(readfile("vape/CustomModules/6872274481.lua"))()
+if isfile("CustomModules/6872274481.lua") then
+    loadstring(readfile("CustomModules/6872274481.lua"))()
 else
-	local publicrepo = vapeGithubRequest("CustomModules/6872274481.lua")
-	if publicrepo then
-		loadstring(publicrepo)()
-	end
+    local publicrepo = vapeGithubRequest("6872274481.lua")
+    if publicrepo then
+        loadstring(publicrepo)()
+    end
 end
